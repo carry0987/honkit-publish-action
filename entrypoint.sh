2f8d4a486f67a7bc01e791ff6cd92e295f0f882b
carry0987 <carry0987@users.noreply.github.com>
2024-10-08 02:42:58 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -49,7 +49,7 @@ pnpm honkit build
 checkIfErr
 ls -al /honkit/_book
 checkIfErr
-cp -rvf /honkit/_book "$BOOK_DIR"/
+cp -rvf /honkit/_book "$BOOK_DIR"/_book
 ls -al "$BOOK_DIR"
 cd - || exit
 echo '[INFO] Finished to build HonKit static files.'
